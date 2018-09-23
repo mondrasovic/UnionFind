3189b3a0c9df2e088536fb004ff48488946f252a
Nightwalker495 <milan.ondrasovic@gmail.com>
2018-09-23 07:33:53 +0200

    Initial commit.

---
+++ DisjointSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnionFind
+{
+	internal class Node<T> where T : System.IComparable<T>
+	{
+		public T Data { get; set; }
+		public Node<T> Parent { get; set; }
+		public int Rank { get; set; }
+
+		public Node(T data)
+		{
+			Data = data;
+			Parent = this;
+			Rank = 0;
+		}
+	}
+
+	public class DisjointSet<T> where T: System.IComparable<T>
+    {
+		private Dictionary<T, Node<T>> nodes;
+
+        public DisjointSet()
+        {
+			nodes = new Dictionary<T, Node<T>>();
+        }
+
+		public bool MakeSet()
+		{
+			return false;
+		}
+    }
+}
